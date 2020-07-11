498942731bb102fb47604300300a9e8f831fef5c
pakhozou <782329086@qq.com>
2020-07-11 18:56:35 +0800

    update-src-compontent-pakhozou

--- src/components/Authority/Operational.js
+++ src/components/Authority/Operational.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import {
   Table,
+  Input,
   Space,
   Button,
-  Popconfirm,
   Row,
-  Col,
-} from 'antd';
-import axios from '../../utils/axios';
-import './css/Authority.css'
+  Radio,
+  Col, Switch,Modal} from 'antd';
+import { UserOutlined, LockOutlined } from '@ant-design/icons';
+
+import './css/Operational.css';
+
 import {inject,observer} from "mobx-react";
-import Roleapi from "../../api/Role";
+
+import Roleapi from '../../api/index';
+import axios from '../../utils/axios';
+
 @inject('data')
 @observer
 //xxx 组件名
@@ -26,12 +31,36 @@ class Operational extends React.Component {
           caozuo: '全部功能',
           status:true,
         }],
-      fenyeobj:{
-        page:1,
-        size:3
-      }
+      //当前页
+      current:1,
+      //每页显示
+      pageSize:3,
+      //总条数
+      total:0,
+      //默认第几页
+      defaultCurrent:1,
+      //是否可以条状
+      showQuickJumper:1,
+
+      //编辑模态框
+      visible: false,
+
+      options : [
+        { label: 1, value: '超级管理员' },
+        { label: 2, value: '普通管理员' },
+        { label: 3, value: '客服' },
+        { label: 4, value: '商品管理员' },
+        { label: 5, value: '用户' },
+      ],
+      editStatus:[],
+      userID:0,
+      roleID : 1,
+
+    //  添加
+      addvisible:false
     }
   }
+
   //分页
   fenye(){
     const fy = {
@@ -56,24 +85,65 @@ class Operational extends React.Component {
     return fy
   };
 
+  changPage(current){
+    console.log(current);
+
+    this.setState({
+      current:current
+    },function () {
+      this.getuserrole()
+    })
+  }
+
+  //判断是否禁用
+  showcaozuo(data){
+    if (data.user_id == 1){
+      data.status =1
+    }
+    let disableStatus = data.status;
+    return disableStatus
+  }
+  //变化回调
+  onShowSizeChange(Current,pageSize){
+    console.log(Current,pageSize)
+  }
+  //获取列表数据
   getuserrole(){
     axios.get(Roleapi.userRole.adminList,
-      {params:{
-          page: this.state.fenyeobj.page,
-          size: this.state.fenyeobj.size
+      {
+        params:{
+          page: this.state.current,
+          size: this.state.pageSize
         }
       }).then((res)=>{
-      // console.log('获取的用户');
-      // console.log(res.data.data.data);
+      console.log('获取的用户');
+      console.log(res);
+      this.setState({
+        total:res.data.data.dataCount
+      });
+      // console.log(this.state.total);
       let xiutime = res.data.data.data.map((item)=>{
         return item.createTime = this.getdata(item.createTime)
       });
       res.data.data.data.createTime = xiutime;
       this.props.data.getuserRole(res.data.data.data)
-
     })
   };
-
+  //是否禁用
+  switchcheck(e){
+    let state = {}
+      state.userId = e.user_id
+      state.statue = e.user_statue
+      // console.log(e.user_id);
+    //       // console.log(e.user_statue);
+    //       // console.log(state);
+    this.props.data.getswitchstatus(state).then((res)=>{
+      if (res == '成功'){
+        this.getuserrole()
+      }
+    })
+  }
+  //时间戳
   getdata(createdate){
     const date=new Date(createdate);
     const year=date.getFullYear();
@@ -84,9 +154,87 @@ class Operational extends React.Component {
     const sec = date.getSeconds();
     return year+"-"+month+"-"+day+ "  "+ hour+":" +min+":"+sec ;
   };
+  //循环获取radio
+  getradio(list){
+    let radiolist = list.map((item)=>{
+      return  <Radio className={'radioStyle'} value={item.label}>{item.value}</Radio>
+    })
+    return radiolist
+  }
+  //编辑
+  Edit=(text)=>{
+    this.setState({
+      visible: true,
+    });
+    console.log(text.user_id);
+    this.props.data.userID = text.user_id;    //用户id
+    this.props.data.roleID = text.role_id;    //角色id
+  };
+  //模态框点击确定函数
+  handleOk = e => {
+    console.log(e);
+    this.setState({
+      visible: false,
+    });
+  };
+  //点击添加
+  showModel=()=>{
+    this.setState({
+      addvisible: true,
+    });
+  }
+  //添加
+  adduser(){
+
+  }
+  //点击取消按钮
+  handleCancel = e => {
+    console.log(e);
+    this.setState({
+      visible: false,
+      addvisible: false,
+    });
+  };
+  //单选按钮change事件
+  onChange = e => {
+   this.props.data.roleID =  e.target.value
+    console.log(this.props.data.roleID);
+  };
+  //点击确定发起请求
+  addRole(){
+    this.setState({
+      visible:false,
+    })
+    axios.post(Roleapi.userRole.userChangeRole,{
+      roleId : this.props.data.roleID,
+      userId : this.props.data.userID
+    },{
+      transformRequest:[
+        function(data){
+          let params = "";
+          var arr = [];
+          for(var key in data){
+            arr.push(key+"="+data[key]);
+          }
+          params = arr.join("&");
+          return params;
+        }
+      ]
+    }).then((res)=>{
+      console.log(res);
+      if (res.data.code == 200){
+        this.getuserrole();
+      }
+    })
+  }
   //挂载前
   componentWillMount() {
-    this.getuserrole()
+    this.getuserrole();   //获取数据
+
+    let options =this.getradio(this.state.options);
+    this.setState({
+      editStatus:options
+    })
   }
 
 //渲染
@@ -100,6 +248,11 @@ class Operational extends React.Component {
         render: text => <a>{text}</a>,
       },
       {
+        title: '管理员登录账号',
+        dataIndex: 'user_username',
+        key: 'user_username',
+      },
+      {
         title: '职位',
         dataIndex: 'role_nameZh',
         key: 'role_nameZh',
@@ -111,18 +264,23 @@ class Operational extends React.Component {
         key: 'createTime',
       },
       {
+        title: '状态',
+        render:(text,record)=>(
+          <Space size="middle">
+            <Switch
+              checkedChildren="开启"
+              unCheckedChildren="关闭"
+              onChange={()=>this.switchcheck(text)}
+              defaultChecked={record.user_statue==1?true:false}/>
+          </Space>
+        )
+      },
+      {
         title: '操作',
         key: 'action',
         render: (text, record,index) => (
           <Space size="middle">
-            <Button type='link' >编辑</Button>
-            <Popconfirm
-              title="确定删除吗？"
-              onConfirm={()=>this.Del(index)}
-              okText="确定"
-              cancelText="取消">
-              <Button type='link' >删除</Button>
-            </Popconfirm>
+            <Button type='link' onClick={()=>{this.Edit(text)}}  disabled={this.showcaozuo(text)}>编辑</Button>
           </Space>
         ),
       },
@@ -134,12 +292,66 @@ class Operational extends React.Component {
             <Button type='primary' onClick={this.showModel}>添加</Button>
           </Col>
         </Row>
+        {/*表格*/}
         <Table
           className='RoleTable'
           columns={columns}
           dataSource={this.props.data.userRolelist}
-          pagination={this.fenye()}/>
+          pagination={this.fenye()}
+        />
+
+        {/*编辑模态框*/}
+        <Modal
+          title="选择角色"
+          visible={this.state.visible}
+          okText='确定'
+          cancelText='取消'
+          onOk={this.addRole.bind(this)}
+          onCancel={this.handleCancel}
+          >
+
+          <Radio.Group  onChange={this.onChange} value={this.props.data.roleID}>
+            {this.state.editStatus}
+          </Radio.Group>
+
+        </Modal>
+
+
+      {/* 添加模态框*/}
+        <Modal
+          title="添加账号"
+          visible={this.state.addvisible}
+          okText='确定'
+          cancelText='取消'
+          onOk={this.adduser.bind(this)}
+          onCancel={this.handleCancel}
+        >
+          <Row gutter={[16, 16]}>
+            <Col span={6} offset={4}>
+              <label htmlFor=""><h3>请输入账号：</h3></label>
+            </Col>
+            <Col span={12}>
+              <Input
+                prefix={<UserOutlined className="site-form-item-icon" />}
+                placeholder="Username"
+              />
+            </Col>
+          </Row>
+          <Row gutter={[16, 16]}>
+            <Col span={6} offset={4}>
+              <label htmlFor=""><h3>请输入密码：</h3></label>
+            </Col>
+            <Col span={12}>
+              <Input
+                prefix={<LockOutlined className="site-form-item-icon" />}
+                type="password" placeholder="Password"/>
+            </Col>
+          </Row>
+
+        </Modal>
       </div>
+
+
     )
   }
 }
